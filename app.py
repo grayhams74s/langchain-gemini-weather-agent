45091c4a7b602a6f8a7177146bb02e51f43a9f52
grayhams74s <felixjunang@gmail.com>
2026-07-22 18:45:57 +0800

    dockerize

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from agent import agent, get_message_text
 import uuid
+import os
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = os.environ["FLASK_SECRET_KEY"]
 
 @app.route('/')
 def home():
